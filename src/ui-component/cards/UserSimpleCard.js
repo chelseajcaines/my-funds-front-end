917d2b90bcbb736dd28f94e63b78392679e95ae5
Chelsea Caines <chelseajcaines@hotmail.com>
2024-11-11 22:11:25 -0330

    removed unnecessary code

--- src/ui-component/cards/UserSimpleCard.js
+++ src/ui-component/cards/UserSimpleCard.js
@@ -1,19 +1,13 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 
 // material-ui
 import { useTheme, styled } from '@mui/material/styles';
-import { Button, Card, Chip, Grid, IconButton, Menu, MenuItem, Typography } from '@mui/material';
+import { Button, Card, Grid, Typography } from '@mui/material';
 
 // project imports
-import Avatar from '../extended/Avatar';
 import { gridSpacing } from 'store/constant';
 
 // assets
-import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { Box } from '@mui/material';
 import LinearProgress from '@mui/material/LinearProgress';
 
@@ -50,67 +44,11 @@ LinearProgressWithLabel.propTypes = {
     value: PropTypes.number
 };
 
-// styles
-const FacebookWrapper = styled(Button)({
-    padding: 8,
-    background: 'rgba(66, 103, 178, 0.2)',
-    '& svg': {
-        color: '#4267B2'
-    },
-    '&:hover': {
-        background: '#4267B2',
-        '& svg': {
-            color: '#fff'
-        }
-    }
-});
-
-const TwitterWrapper = styled(Button)({
-    padding: 8,
-    background: 'rgba(29, 161, 242, 0.2)',
-    '& svg': {
-        color: '#1DA1F2'
-    },
-    '&:hover': {
-        background: '#1DA1F2',
-        '& svg': {
-            color: '#fff'
-        }
-    }
-});
-
-const LinkedInWrapper = styled(Button)({
-    padding: 8,
-    background: 'rgba(14, 118, 168, 0.12)',
-    '& svg': {
-        color: '#0E76A8'
-    },
-    '&:hover': {
-        background: '#0E76A8',
-        '& svg': {
-            color: '#fff'
-        }
-    }
-});
-
 // ==============================|| USER SIMPLE CARD ||============================== //
 
 const UserSimpleCard = ({ avatar }) => {
-    const theme = useTheme();
-    const avatarProfile = avatar && avatarImage(`./${avatar}`);
-
     return (
-        <Card
-        // sx={{
-        //     p: 2,
-        //     background: theme.palette.mode === 'dark' ? theme.palette.dark.main : theme.palette.grey[50],
-        //     border: '1px solid',
-        //     borderColor: theme.palette.mode === 'dark' ? 'transparent' : theme.palette.grey[100],
-        //     '&:hover': {
-        //         border: `1px solid${theme.palette.primary.main}`
-        //     }
-        // }}
-        >
+        <Card>
             <Grid container spacing={gridSpacing}>
                 <Grid item xs={12}>
                     <Grid container spacing={gridSpacing}>
